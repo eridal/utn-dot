d347fa4261d9065b68f2b064a7ebb86c3afb2268
Martin Scotta <martinscotta@gmail.com>
2014-04-19 15:18:22 -0300

    compress css

--- build/styles.php
+++ build/styles.php
@@ -5,10 +5,13 @@ require __DIR__ . '/../vendor/autoload.php';
 use Assetic\Asset\AssetCollection;
 use Assetic\Asset\FileAsset;
 use Assetic\Asset\GlobAsset;
+use Assetic\Filter\CssMinFilter;
 
 $css = new AssetCollection(array(
     new GlobAsset(__DIR__ . '/../assets/*.css'),
     new GlobAsset(__DIR__ . '/../css/*.css'),
+), array(
+    new CssMinFilter()
 ));
 
 file_put_contents(__DIR__ . '/styles.css', $css->dump());
