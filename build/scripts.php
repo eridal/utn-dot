d40514feed486fa415b1e08c0277538add7d1c00
Martin Scotta <martinscotta@gmail.com>
2014-04-19 15:13:34 -0300

    faster load

--- build/scripts.php
+++ build/scripts.php
@@ -5,6 +5,7 @@ require __DIR__ . '/../vendor/autoload.php';
 use Assetic\Asset\AssetCollection;
 use Assetic\Asset\FileAsset;
 use Assetic\Asset\GlobAsset;
+use Assetic\Filter\JSMinFilter;
 
 $css = new AssetCollection(array(
 
@@ -13,12 +14,13 @@ $css = new AssetCollection(array(
     new FileAsset(__DIR__ . '/../assets/almond.js'),
     new FileAsset(__DIR__ . '/../assets/backbone.js'),
     new FileAsset(__DIR__ . '/../assets/bootstrap.js'),
-    new FileAsset(__DIR__ . '/../assets/viz.js'),
 
     new GlobAsset(__DIR__ . '/../js/coll/*.js'),
     new GlobAsset(__DIR__ . '/../js/model/*.js'),
     new GlobAsset(__DIR__ . '/../js/util/*.js'),
     new GlobAsset(__DIR__ . '/../js/view/*.js'),
+), array(
+    new JSMinFilter()
 ));
 
 file_put_contents(__DIR__ . '/scripts.js', $css->dump());
